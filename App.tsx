f0dbb479a4923775e7f5ca6197800acd7a00a404
bucky <buckyhelmsmash@gmail.com>
2023-09-08 23:23:25 +0700

    theming

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import {Button} from 'react-native';
 import {AuthProvider, useAuth} from "./app/context/AuthContext";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {NavigationContainer} from "@react-navigation/native";
 import Home from "./app/screens/Home";
 import Login from "./app/screens/Login";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Button, useTheme} from "@react-native-material/core";
 
 const queryClient = new QueryClient({
     defaultOptions: {queries: {retry: 2}},
@@ -24,10 +24,16 @@ const Stack = createNativeStackNavigator()
 
 export const Layout = () => {
     const {authState, onLogout} = useAuth()
+    const theme = useTheme()
 
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+                screenOptions={{
+                    statusBarStyle: "dark",
+                    statusBarColor: theme.palette.primary.main
+                }}
+            >
                 {
                     authState?.authenticated
                         ? (
@@ -43,6 +49,7 @@ export const Layout = () => {
                             <Stack.Screen
                                 name={"Login"}
                                 component={Login}
+
                             />
                         )
                 }
